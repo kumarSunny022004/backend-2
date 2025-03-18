3f29ca926715c32edd25a0de72ca522eaa9b9860
KUMARSUNNY <ks9107163@gmail.com>
2025-03-18 10:41:40 +0530

    added some features

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({path: "./env"});
 
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+         console.log("Error: ", error);
+            throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at http://localhost:${process.env.PORT}`)
+    })
+}).catch((error)=>{
+    console.log("MOngoDB connection error: ", error);
+})
+
 
 
 
